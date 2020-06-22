16068c5a33d7bf33c9b8f7d4204f57b51ae576fc
sachin-in1 <saching1998@cet.ac.in>
2020-06-23 00:10:07 +0530

    dashboard fixes + edit feature

--- src/Components/Dashboard/Profile.js
+++ src/Components/Dashboard/Profile.js
@@ -1,26 +1,64 @@
 import React from 'react'
 
-
 class Profile extends React.Component{
     constructor(props){
         super(props);
-        this.state = {profileval:{ 
-            image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/New_york_times_square-terabass.jpg/1200px-New_york_times_square-terabass.jpg",
-            name:"sachin",
-            email:"sachinhsf<sghfjh@abc.com"}}
+       this.state = {profileval: {name:"sachin",email:"sachin@abc.com",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/New_york_times_square-terabass.jpg/1200px-New_york_times_square-terabass.jpg",
+    },
+        edit:false};
+            this.handleSubmit = this.handleSubmit.bind(this);
+            this.changeEdit = this.changeEdit.bind(this);
+    }
+    changeEdit(event){
+        this.setState({
+            edit : !this.state.edit
+        });
+    }
+    handleSubmit(event){
+        if(this._inputEmail.value !== '' && this._inputName.value !== ''){
+            alert("Are You Sure?");
+            this.setState({
+                profileval : {name:this._inputName.value !== this.state.profileval.name?this._inputName.value:this.state.profileval.name,email:this._inputEmail.value!==this.state.profileval.email?this._inputEmail.value:this.state.profileval.email,image:this.state.profileval.image}
+            });
+        }
+            
+        event.preventDefault();
+        this.log();
+    }
+    log(){
+        console.log(this.state.profileval);
+        
     }
     render(){
+        
         return (
-        <div className="profile">
+        
+        <div className="profile center">
             <img src={this.state.profileval.image} className="Profile-pic" alt="logo" />
-            <img src={this.state.profileval.image} className="Profile-pic" alt="logo" /><br/><br/>
-            <div className="profile-details">
-                <h2>Name:</h2><input value={this.state.profileval.name} onChange={this.handleChange}></input>
+            <a onClick={this.changeEdit}><img src={this.state.profileval.image} className="Profile-pic" alt="logo" /></a>
+            <br/><br/>
+            {/* conditional */}
+            { this.state.edit ===true ?  
+            <div className="profile-details-edit">
+            <form onSubmit={this.handleSubmit} id="myForm" className="form" >
+                {/* onChange={this.handleChange} */}
+                <h2>Name:</h2>
+                <input type="text" ref={(a) => this._inputName = a}  defaultValue={this.state.profileval.name} placeholder={this.state.profileval.name} />
                 <br/><br/>
-                <h2>Email:</h2><input type="text" defaultValue={this.state.profileval.email}></input>
-            </div>
-        </div>
-        );
+                <h2>Email:</h2>
+                <input type="text" ref={(a) => this._inputEmail = a} defaultValue={this.state.profileval.email} /><br/><br/>
+                <input type="submit" value="Submit"/>
+            </form></div>:
+            <div className="profile-details">
+                
+                <h2>Name:</h2><input type="text" defaultValue={this.state.profileval.name}></input>
+                <br/><br/>
+                <h2>Email:</h2><input type="text" defaultValue={this.state.profileval.email}></input><br/><br/>
+                
+            </div>}
+            {/* conditional */}
+            
+        </div>);
     }
 }
 export default Profile; 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function Navbar() {
     return (
-        <div class="navbar">
+        <div className="navbar">
             <a href="/" className="nav-item">Home</a>
             <a href="/dashboard" className="nav-item">Dashboard</a>
         </div>
