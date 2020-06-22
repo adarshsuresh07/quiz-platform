c83c5475f3543f463b88bc553e6acbac6fe411ca
sachin-in1 <saching1998@cet.ac.in>
2020-06-22 17:38:46 +0530

    image + mobile view

--- src/Components/Dashboard/Profile.js
+++ src/Components/Dashboard/Profile.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import logo from '../../logo.svg';
 
 
 class Profile extends React.Component{
     constructor(props){
         super(props);
-        this.state = {profileval:{ name:"sachin",email:"sachin@abc.com"}}
+        this.state = {profileval:{ 
+            image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/New_york_times_square-terabass.jpg/1200px-New_york_times_square-terabass.jpg",
+            name:"sachin",
+            email:"sachinhsf<sghfjh@abc.com"}}
     }
     render(){
         return (
         <div className="profile">
-            <img src={logo} className="Profile-pic" alt="logo" />
+            <img src={this.state.profileval.image} className="Profile-pic" alt="logo" />
+            <img src={this.state.profileval.image} className="Profile-pic" alt="logo" /><br/><br/>
             <div className="profile-details">
-                {this.state.profileval.name}
-                <br/>
-                {this.state.profileval.email}
+                <h2>Name:</h2><input value={this.state.profileval.name} onChange={this.handleChange}></input>
+                <br/><br/>
+                <h2>Email:</h2><input type="text" defaultValue={this.state.profileval.email}></input>
             </div>
         </div>
         );
